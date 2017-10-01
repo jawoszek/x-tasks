324ca7fc52ad0c85addd9530f0db1df15a31fee2
Kacper <jawoszek.kacper@gmail.com>
2017-10-01 13:42:18 +0200

    More complex ordering testing.

--- ordering-system/src/test/java/com/jawoszek/xtasks/ordering/ApplicationTest.java
+++ ordering-system/src/test/java/com/jawoszek/xtasks/ordering/ApplicationTest.java
@@ -11,14 +11,19 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import static java.lang.String.format;
+import static java.util.Arrays.stream;
 
 /**
  * @author Kacper
  */
 @RunWith(MockitoJUnitRunner.class)
 public class ApplicationTest {
+
+    private static final String ZERO_PRICE = "0.00$";
+    private static final String ONE_LUNCH_PRICE = "12.99$";
 
     @Mock
     private PrintStream mockPrintStream;
@@ -42,7 +47,7 @@ public class ApplicationTest {
     @Test
     public void startWithEmptyCancelledOrder() {
         // given
-        Scanner scanner = new Scanner(getInputStream(format("0%n3%n0%n2")));
+        Scanner scanner = new Scanner(getInputStream(controlSequence(0, 3, 0, 2)));
         Console console = new Console(mockPrintStream, scanner);
         Menu menu = Menu.standardMenu();
         LinesVerifier verifier = new LinesVerifier(mockPrintStream);
@@ -52,10 +57,37 @@ public class ApplicationTest {
 
         // then
         verifier.expectNewOrder();
-        verifier.expectOrderControl();
-        verifier.expectCancelOrder();
+        verifier.expectOrderControl(ZERO_PRICE);
+        verifier.expectCancelOrder(ZERO_PRICE);
         verifier.expectNewOrder();
         verifier.verifyExpectedLinesInOutput();
+    }
+
+    @Test
+    public void startWithSingleLunchAccepted() {
+        Scanner scanner = new Scanner(getInputStream(controlSequence(0, 1, 0, 0, 2, 0, 2)));
+        Console console = new Console(mockPrintStream, scanner);
+        Menu menu = Menu.standardMenu();
+        LinesVerifier verifier = new LinesVerifier(mockPrintStream);
+
+        // when
+        new Application().start(console, menu);
+
+        // then
+        verifier.expectNewOrder();
+        verifier.expectOrderControl(ZERO_PRICE);
+        verifier.expectModifyOrder(ZERO_PRICE);
+        verifier.expectAddLunch(ZERO_PRICE);
+        verifier.expectOrderControl(ONE_LUNCH_PRICE);
+        verifier.expectAcceptOrder(ONE_LUNCH_PRICE);
+        verifier.expectNewOrder();
+        verifier.verifyExpectedLinesInOutput();
+    }
+
+    private String controlSequence(int... actions) {
+        return stream(actions)
+                .mapToObj(Integer::toString)
+                .collect(Collectors.joining(format("%n")));
     }
 
     private InputStream getInputStream(String content) {
--- ordering-system/src/test/java/com/jawoszek/xtasks/ordering/LinesVerifier.java
+++ ordering-system/src/test/java/com/jawoszek/xtasks/ordering/LinesVerifier.java
@@ -3,6 +3,7 @@ package com.jawoszek.xtasks.ordering;
 import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 import static java.util.stream.Stream.of;
@@ -13,16 +14,27 @@ import static org.mockito.Mockito.*;
  */
 public class LinesVerifier {
 
-    private static final String ZERO_PRICE_HEADER = "=== Current order price: 0.00$ ===";
+    private static final String PRICE_HEADER_FORMAT = "=== Current order price: %s ===";
     private static final String MAIN_MENU =
             format("%s%n%s%n%s", "0 - Create new order", "1 - Show menu", "2 - Exit application");
     private static final String ORDER_MENU =
             format("%s%n%s%n%s%n%s", "0 - Show order", "1 - Modify order", "2 - Accept order", "3 - Cancel order");
+    private static final String MODIFY_MENU =
+            format("%s%n%s%n%s%n%s", "0 - Add lunch", "1 - Add drink", "2 - Remove lunch", "3 - Remove drink");
+    private static final String LUNCH_MENU = of(
+            "0 - Main dish:pork chop with potatoes   Dessert:apple pie   Price:12.99$",
+            "1 - Main dish:ruthenian pierogi   Dessert:apple pie   Price:10.99$",
+            "2 - Main dish:nachos   Dessert:chocolate cake   Price:15.99$",
+            "3 - Main dish:tacos with pork and cheese   Dessert:chocolate cake   Price:13.99$",
+            "4 - Main dish:pizza margherita   Dessert:chocolate cake   Price:18.99$",
+            "5 - Main dish:farfalle with tomato sauce   Dessert:chocolate cake   Price:12.99$"
+    ).collect(Collectors.joining(format("%n")));
     private static final String OPTIONS_HEADER = format("%nPossible options:");
     private static final String CONTINUE_MENU = "0 - Continue";
     private static final String OPTIONS_REQUEST = "Please choose valid number from options above";
     private static final String CLEAR_SCREEN = format("%n%n%n");
     private static final String CANCELLED = "Order cancelled";
+    private static final String ACCEPTED = "Order accepted";
     private static final String MAIN_MENU_HEADER = "Welcome to ordering system!";
 
     private final Map<String, Integer> expectedLines = new HashMap<>();
@@ -37,13 +49,28 @@ public class LinesVerifier {
                 .forEach(this::expectLine);
     }
 
-    public void expectOrderControl() {
-        of(ZERO_PRICE_HEADER, OPTIONS_HEADER, ORDER_MENU, OPTIONS_REQUEST, CLEAR_SCREEN)
+    public void expectOrderControl(String price) {
+        of(priceLine(price), OPTIONS_HEADER, ORDER_MENU, OPTIONS_REQUEST, CLEAR_SCREEN)
                 .forEach(this::expectLine);
     }
 
-    public void expectCancelOrder() {
-        of(ZERO_PRICE_HEADER, OPTIONS_HEADER, CONTINUE_MENU, OPTIONS_REQUEST, CANCELLED, CLEAR_SCREEN)
+    public void expectCancelOrder(String price) {
+        of(priceLine(price), OPTIONS_HEADER, CONTINUE_MENU, OPTIONS_REQUEST, CANCELLED, CLEAR_SCREEN)
+                .forEach(this::expectLine);
+    }
+
+    public void expectModifyOrder(String price) {
+        of(priceLine(price), OPTIONS_HEADER, MODIFY_MENU, OPTIONS_REQUEST, CLEAR_SCREEN)
+                .forEach(this::expectLine);
+    }
+
+    public void expectAddLunch(String price) {
+        of(priceLine(price), OPTIONS_HEADER, LUNCH_MENU, OPTIONS_REQUEST, CLEAR_SCREEN)
+                .forEach(this::expectLine);
+    }
+
+    public void expectAcceptOrder(String price) {
+        of(priceLine(price), OPTIONS_HEADER, CONTINUE_MENU, OPTIONS_REQUEST, ACCEPTED, CLEAR_SCREEN)
                 .forEach(this::expectLine);
     }
 
@@ -54,5 +81,9 @@ public class LinesVerifier {
 
     private void expectLine(String line) {
         expectedLines.merge(line, 1, (old, added) -> old + added);
+    }
+
+    private String priceLine(String price) {
+        return format(PRICE_HEADER_FORMAT, price);
     }
 }
