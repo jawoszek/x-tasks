539d8eeb463e3fcf81d1ecd69bb160ea7249b9b6
Kacper <jawoszek.kacper@gmail.com>
2017-10-01 13:05:11 +0200

    More MineSweeper tests.

--- mine-sweeper/src/test/java/minesweeper/MineSweeperImplTest.java
+++ mine-sweeper/src/test/java/minesweeper/MineSweeperImplTest.java
@@ -11,10 +11,63 @@ public class MineSweeperImplTest {
 
     @Test
     public void minefield(){
-        // given
-        MineSweeper sweeper = new MineSweeperImpl();
         String input = "*...\n..*.\n....";
         String expectedOutput = "*211\n12*1\n0111";
+
+        testMinefield(input, expectedOutput);
+    }
+
+    @Test
+    public void minefieldWithSingleMineCell(){
+        String input = "*";
+        String expectedOutput = "*";
+
+        testMinefield(input, expectedOutput);
+    }
+
+    @Test
+    public void minefieldWithSingleEmptyField(){
+        String input = ".";
+        String expectedOutput = "0";
+
+        testMinefield(input, expectedOutput);
+    }
+
+    @Test
+    public void minefieldWithMineSquare(){
+        String input = "**\n**";
+        String expectedOutput = "**\n**";
+
+        testMinefield(input, expectedOutput);
+    }
+
+    @Test
+    public void minefieldWithEmptySquare(){
+        String input = "..\n..";
+        String expectedOutput = "00\n00";
+
+        testMinefield(input, expectedOutput);
+    }
+
+    @Test
+    public void minefieldWithOneMineInSquare(){
+        String input = "..\n.*";
+        String expectedOutput = "11\n1*";
+
+        testMinefield(input, expectedOutput);
+    }
+
+    @Test
+    public void minefieldWithTwoMinesInSquare(){
+        String input = "*.\n.*";
+        String expectedOutput = "*2\n2*";
+
+        testMinefield(input, expectedOutput);
+    }
+
+    private static void testMinefield(String input, String expectedOutput){
+        // given
+        MineSweeper sweeper = new MineSweeperImpl();
 
         // when
         sweeper.setMineField(input);
